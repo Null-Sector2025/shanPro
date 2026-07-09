ad7af3341f26cb4ffa0030b86f5535ff0d51f42e
Null-Sector2025 <2734952662@qq.com>
2026-07-09 16:25:30 +0800

    修复CS1508 XAML资源重复冲突，修复CS8602空引用警告

--- Models/TerminalSession.cs
+++ Models/TerminalSession.cs
@@ -5,7 +5,6 @@ namespace shanPro.Models;
 
 public class TerminalSession
 {
-    // 明确指定 Android.OS.Environment，消除歧义
-    public string WorkDir { get; set; } = Android.OS.Environment.ExternalStorageDirectory.AbsolutePath;
+    public string WorkDir { get; set; } = Android.OS.Environment.ExternalStorageDirectory?.AbsolutePath ?? "/storage/emulated/0";
     public List<string> CommandHistory { get; set; } = new();
 }
